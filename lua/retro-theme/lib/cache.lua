d3b476de756a2c0ffe1ad621c12cc9cf422d241a
Marco Kellershoff <marco@kellershoff.net>
2025-01-28 06:15:44 +0100

    fix(fs): use .cache dir for read-only fs

--- lua/retro-theme/lib/cache.lua
+++ lua/retro-theme/lib/cache.lua
@@ -1,5 +1,14 @@
 local config = require("retro-theme").config
 local path = require("retro-theme.lib.path")
+local base_cache_dir = vim.fn.stdpath("cache") .. "/retro-theme"
+local nested_cache_dir = base_cache_dir .. "/retro-theme"
+
+-- Ensure the directories exist
+if vim.fn.isdirectory(nested_cache_dir) == 0 then
+  vim.fn.mkdir(nested_cache_dir, "p")
+end
+
+local cache_path = nested_cache_dir .. "/cache.lua"
 
 local M = {}
 
@@ -7,7 +16,7 @@ M.clear = function()
   if config.disable_cache or config.hot_reload then
     return
   end
-  if vim.fn.filereadable(path.cache) == 1 then
+  if vim.fn.filereadable(cache_path) == 1 then
     assert(os.remove(path.cache))
   end
 end
@@ -16,7 +25,7 @@ M.exists = function()
   if config.disable_cache or config.hot_reload then
     return false
   end
-  return vim.fn.filereadable(path.cache) == 1
+  return vim.fn.filereadable(cache_path) == 1
 end
 
 M.write = function(colors)
@@ -25,7 +34,7 @@ M.write = function(colors)
   end
   local serpent = require("retro-theme.lib.serpent")
   local str = serpent.dump(colors)
-  local file = io.open(path.cache, "wb")
+  local file = io.open(cache_path, "wb")
   file:write(str)
   file:close()
 end
@@ -34,7 +43,21 @@ M.read = function()
   if config.disable_cache or config.hot_reload then
     return nil
   end
-  local colors = require("retro-theme.cache.default")
+
+  local colors
+  if vim.fn.filereadable(cache_path) == 1 then
+    package.path = package.path
+      .. ";"
+      .. base_cache_dir
+      .. "/?.lua;"
+      .. base_cache_dir
+      .. "/?/init.lua;"
+      .. nested_cache_dir
+      .. "/?.lua"
+
+    colors = require("retro-theme.cache")
+  end
+
   if not colors then
     return nil
   end
--- lua/retro-theme/lib/path.lua
+++ lua/retro-theme/lib/path.lua
@@ -1,8 +1,9 @@
-local config = require("retro-theme").config
-
 local lua_path = vim.fs.dirname(debug.getinfo(1).source:sub(2)) .. "/.."
-local compile_path = lua_path .. "/cache"
-local cache_path = compile_path .. "/default.lua"
+local cache_dir = vim.fn.stdpath("cache") .. "/retro-theme"
+if vim.fn.isdirectory(cache_dir) == 0 then
+  vim.fn.mkdir(cache_dir, "p")
+end
+local cache_path = cache_dir .. "/cache.lua"
 local palette_colors_path = lua_path .. "/palette/colors.lua"
 
 local M = {}
